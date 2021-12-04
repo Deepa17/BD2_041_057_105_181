f540fb33c543877edc46d2c6ea5c74034f7c3a3c
Deepa17 <deepuvenkat175@gmail.com>
2021-12-04 17:31:18 +0530

    manual label encoding of categorical cols

--- preprocess.py
+++ preprocess.py
@@ -7,9 +7,11 @@ from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
 import json
 import numpy as np
-#import pyspark.sql.functions as F
+
 from pyspark.sql.functions import to_timestamp
 from pyspark.sql.functions import hour, month, year
+
+from pyspark.sql.functions import col , udf
 
 from pyspark.ml.feature import HashingTF, IDF, Tokenizer
 from pyspark.ml.feature import CountVectorizer
@@ -34,46 +36,49 @@ schema = StructType([
     StructField("feature8",DoubleType())
 ])
 
+categories = {'FRAUD':0,'SUICIDE':1,'SEX OFFENSES FORCIBLE':2,
+'LIQUOR LAWS':3,'SECONDARY CODES':4,'FAMILY OFFENSES':5,'MISSING PERSON':6,
+'OTHER OFFENSES':7,'DRIVING UNDER THE INFLUENCE':8,'WARRANTS':9,
+'ARSON':10,'SEX OFFENSES NON FORCIBLE':11,'FORGERY/COUNTERFEITING':12,
+'GAMBLING':13,'BRIBERY':14,'ASSAULT':15,'DRUNKENNESS':16,
+'EXTORTION':17,'TREA':18,'WEAPON LAWS':19,'LOITERING':20,
+'SUSPICIOUS OCC':21,'ROBBERY':22,'PROSTITUTION':23,
+'EMBEZZLEMENT':24,'BAD CHECKS':25,'DISORDERLY CONDUCT':26,
+'RUNAWAY':27,'RECOVERED VEHICLE':28,'VANDALISM':29,'DRUG/NARCOTIC':30,
+'PORNOGRAPHY/OBSCENE MAT':31,'TRESPASS':32,'VEHICLE THEFT':33,
+'NON-CRIMINAL':34,'STOLEN PROPERTY':35,'LARCENY/THEFT':36,'KIDNAPPING':37,
+'BURGLARY':38}
+
+district = {'MISSION':0,'BAYVIEW':1,'CENTRAL':2,'TARAVAL':3,
+'TENDERLOIN':4,'INGLESIDE':5,'PARK':6,'SOUTHERN':7,
+'RICHMOND':8,'NORTHERN':9}
+
+days = {'Wednesday':0,'Tuesday':1,'Friday':2,'Thursday':3,
+'Saturday':4,'Monday':5,'Sunday':6}
+
+
 #splitting the data into test train set
 def test_train(X,y):
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
   return(X_train, X_test, y_train, y_test)
 
-#label encoding the categorical variables
-def label_encode(df,feature,output_feature):
-  encoder = StringIndexer(inputCol=feature,outputCol=output_feature)
-  df = encoder.fit(df).transform(df)
-  return df
-
-#tokenizing the address
-def tokenize(df,feature,output_feature):
-  tokenizer = Tokenizer(inputCol=feature, outputCol="words")
-  df = tokenizer.transform(df)
-
-  hashingTF = HashingTF(inputCol="words", outputCol=output_feature)
-  df = hashingTF.transform(df)
-
-  df = df.drop("words")
-  return df
 
 #nb classifier
 def naive_bayes(X_train, X_test, y_train, y_test,classes):
   from sklearn.naive_bayes import GaussianNB
   nb = GaussianNB()
-  nb.partial_fit(X_train,y_train,classes)
+  nb.partial_fit(X_train,y_train,classes = classes)
   y_pred = nb.predict(X_test)
-  metrics(y_pred,y_test)
+  metrics(y_pred,y_test,classes)
 
 #to return the metrics of the model 
-def metrics(y_pred,y_true):
+def metrics(y_pred,y_true,classes):
   from sklearn.metrics import accuracy_score
   from sklearn.metrics import classification_report
 
-  target = list(np.unique(y_true))
-  print(target)
   print("Accuracy: ",accuracy_score(y_pred,y_true))
   print("Classification_report:")
-  print(classification_report(y_true,y_pred,labels=target))
+  print(classification_report(y_true,y_pred,labels = classes))
 
 #function to read the stream
 def readMyStream(rdd) :
@@ -82,34 +87,17 @@ def readMyStream(rdd) :
   #print("line:",line)
   #create a df
   df = spark.createDataFrame(data=json.loads(line[0]).values(), schema=schema)
-  #df = spark.createDataFrame(data = spark.read.json(line[0]),schema=schema)--> this didnt work
 
-  #select the required cols
-  #columns = ['feature0','feature1','feature3','feature4','feature6','feature7','feature8']
-  #df = df.select([col for col in columns])
-
-  #encode the catergorical features
-  categorical = ["feature1","feature3","feature4"]
-  #renamed = ["category","day","district"]
-  df = label_encode(df,"feature1","category")
-  df = label_encode(df,"feature3","day")
-  df = label_encode(df,"feature4","district")
-  #drop the categorical cols
-  df = df.drop(*categorical)
+  categ_func = udf(lambda row : categories.get(row,row))
+  df = df.withColumn("feature1", categ_func(col("feature1")))
+  district_func = udf(lambda row : district.get(row,row))
+  df = df.withColumn("feature4", district_func(col("feature4")))
+  day_func = udf(lambda row : days.get(row,row))
+  df = df.withColumn("feature3", day_func(col("feature3")))
 
   
-  #df = tokenize(df,"feature6","Address")
-  #df = df.drop("feature6")
-  #series = df.select(['Address']).rdd.map(lambda x : np.array(x.toArray())).as_matrix().reshape(-1,1)
-  #op= np.appply_along_axis(lambda x:x[0],1,series)
-  #vector_udf = udf(lambda vector: list(vector.toArray()))
-  #df.select("Address").show()
-  #vectors =  df.select("Address").rdd.map(lambda row: row.features.toArray())
   cols = df.columns
-  #df.withColumn("Address",vector_udf((df['Address'])))
-  #print(vectors)
-  #df.rdd.mapValues(lambda x: [x.toArray()]).toDF(cols)
-  #df.show()
+  
   
   df = df.withColumn("timestamp",to_timestamp(df.feature0))
   df=df.withColumn("Hour",hour(df.timestamp)).withColumn("Month",month(df.timestamp)).withColumn("Year",year(df.timestamp))
@@ -123,20 +111,24 @@ def x_y(rdd):
   #print("DataFrame:")
   df.show()
 
-  x_col = ['feature7','feature8','day','district','Hour','Month','Year']
+  x_col = ['feature3','feature4','feature7','feature8','Hour','Month','Year']
   X = data = df.select([col for col in x_col])
-  y = df.select('category')
+  y = df.select('feature1')
   X = np.asarray(X.collect())
   y = np.asarray(y.collect())
-  print(X)
-  print(y)
+  #li = (np.unique(y))
+  #for i in li:
+  #  print(type(i))
+  #print(X)
+  #print(y)
+  print(np.unique(y))
   return(X,y)
 
 def model_train(rdd):
   if not rdd.isEmpty():
     X,y = x_y(rdd)
-    classes = list(np.unique(y))
-    print("Length:",len(classes))
+    classes = [str(i) for i in range(39)]
+    print("classes: ",classes)
     X_train, X_test, y_train, y_test=test_train(X,y)
     naive_bayes(X_train, X_test, y_train, y_test,classes)
 
